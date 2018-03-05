0a3042171e450659dec3e717e12c02f1fef8e691
profguetler <gua@o365.htl-leoben.at>
2018-03-05 16:47:41 +0100

    for-schleife erstellt

---
+++ forSchleife.py
@@ -0,0 +1,14 @@
+# einfache For-Schleife
+
+for i in range(1,10):
+    print("Hallo!", end=" * ")
+print()
+
+name = "Willkommen an der HTL Leoben"
+for z in name:
+    print("=", end="")
+print()
+print(name)
+for z in name:
+    print("=", end="")
+print()
